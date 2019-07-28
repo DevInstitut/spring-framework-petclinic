fa97486fbe6376c8b0efa431ac703dfc0537d5f0
Rossi Oddet <rossi.oddet@dev-institut.fr>
2019-07-28 14:50:24 +0200

    okiii

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -30,7 +30,7 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Inteer id;
+    protected Integer id;
 
     public Integer getId() {
         return id;
